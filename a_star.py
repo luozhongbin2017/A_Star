9fcb7084a7851483e5d5adb5bc1a8f712542766b
ojferro <ojferro@gmail.com>
2018-11-14 23:28:33 -0800

    Bug fixes

--- a_star.py
+++ a_star.py
@@ -3,23 +3,34 @@ import time
 class A_Star:
     def __init__(self):
         self.grid = [
-            [1,1,1,1,1,255,1,1,1,1],
-            [1,1,1,1,1,255,1,1,1,1],
-            [1,1,1,1,1,255,1,1,1,1],
-            [1,1,1,1,1,1,255,1,1,1],
-            [1,1,1,1,1,1,1,255,1,255],
-            [1,1,1,1,1,1,1,1,1,1],
-            [1,1,1,1,1,1,1,1,1,1],
-            [1,1,1,1,1,1,1,1,1,1],
-            [1,1,1,1,1,1,1,1,1,1],
-            [1,1,1,1,1,1,1,1,1,1],
+            [1,1,1,1,1,255,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+            [1,1,1,1,1,255,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+            [1,1,1,1,1,255,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+            [1,1,1,1,1,255,255,1,1,1,1,1,1,1,1,1,1,1,1,1],
+            [1,1,1,1,1,1,255,255,255,255,1,1,1,1,1,1,1,1,1,1],
+            [1,1,1,1,1,1,1,255,1,1,1,1,1,1,1,1,1,1,1,1],
+            [1,1,1,1,1,1,1,255,1,1,1,1,255,255,255,255,255,255,255,255],
+            [1,1,1,1,1,1,1,255,1,1,1,1,1,1,1,1,1,1,1,1],
+            [1,1,1,1,1,1,1,255,1,1,1,1,1,1,1,1,1,1,1,1],
+            [1,1,1,1,1,1,1,255,1,1,1,1,1,1,1,1,1,1,1,1],
+            [1,1,1,1,1,1,1,255,1,1,1,1,1,1,1,1,1,1,1,1],
+            [1,1,1,1,1,1,1,255,1,1,1,1,1,1,1,1,1,1,1,1],
+            [1,1,1,1,1,1,1,255,255,255,255,255,1,1,1,1,1,1,1,1],
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+            [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
         ]
         self.open_set = {} #cell (row, col) : cost
         self.closed_set = {} # cell (row:col) : parent (row, col)
-        self.start = (9,0)
-        self.goal = (0,9)
+        self.start = (19,0)
+        self.goal = (0,19)
         self.wall_value = 255
         self.path = []
+        self.parent_dict = {}
         self.alpha = 1
         self.beta = 1
         self.h_type = "manhattan"
@@ -39,12 +50,14 @@ class A_Star:
                         and curr_cell[0]+di in range (0, len(self.grid))
                         and curr_cell[1]+dj in range (0, len(self.grid[0]))
                         and self.grid[curr_cell[0]+di][curr_cell[1]+dj] != self.wall_value):
-                            self.open_set[(curr_cell[0]+di, curr_cell[1]+dj)] = self.alpha*self.grid[curr_cell[0]+di][curr_cell[1]+dj]+self.beta*self.heuristic((curr_cell[0]+di, curr_cell[1]+dj))
+                            cost = self.alpha*self.grid[curr_cell[0]+di][curr_cell[1]+dj]
+                            heur = self.beta*self.heuristic((curr_cell[0]+di, curr_cell[1]+dj))
+                            #print ("Cost: {}, Heur:{}".format(cost,heur))
+                            self.open_set[(curr_cell[0]+di, curr_cell[1]+dj)] = cost + heur
+                            self.parent_dict[(curr_cell[0]+di, curr_cell[1]+dj)] = curr_cell
+                            
 
     def traverse(self, curr_cell):
-        if not self.open_set:   #no path found
-            return
-
         next_cell = min(self.open_set, key=self.open_set.get)
         self.closed_set[next_cell] = curr_cell
         del self.open_set[next_cell]
@@ -57,6 +70,8 @@ class A_Star:
         while curr_cell != self.goal:   
             self.check_neighbours(curr_cell)
             curr_cell = self.traverse(curr_cell)
+            if not self.open_set:
+                break
 
         if curr_cell == self.goal:
             print ("Goal found!")
@@ -64,20 +79,22 @@ class A_Star:
             print ("No path from start to goal")
 
         while curr_cell is not self.start:
-            self.path.append(self.closed_set[curr_cell])
-            curr_cell = self.closed_set[curr_cell]
+            #self.path.append(self.closed_set[curr_cell])
+            self.path.append(self.parent_dict[curr_cell])
+            curr_cell = self.parent_dict[curr_cell]
         self.path.reverse()
-        
+
         self.draw_path()
 
     def draw_path(self):
 
         #Print path
-        print("Path:")
+        print("Path:", end="")
         for p in self.path:
-            print("-->{}".format(p), end="")
-        print("\n\n")
+            print("{}-->".format(p), end="")
+        print("{}\n\n".format(self.goal))
 
+        #Draw path
         padding =  len(str(len(self.path)))+2
         for row in range(len(self.grid)):
             for col in range(len(self.grid[row])):
